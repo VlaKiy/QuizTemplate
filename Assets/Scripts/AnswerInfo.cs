27e8dac3b387857de70bce7d6a90c6f796fc75e4
Vlad Bistreckiy <vlad.zero07@mail.ru>
2023-02-08 13:39:18 +0500

    Made scoring and showing the next question
    Made scoring for clicking on the button and showing the next question. More comments have been added

--- Assets/Scripts/AnswerInfo.cs
+++ Assets/Scripts/AnswerInfo.cs
@@ -3,20 +3,29 @@ using UnityEngine.UI;
 
 public class AnswerInfo : MonoBehaviour
 {
-    public string ask;
-    public string[] answers;
-    public int rightAnswer;
+    // Следующий контейнер с вопросами и ответами, который нужно будет показать.
+    public GameObject nextAsk;
 
+    // Вопрос
+    public string ask;
+
+    // Список ответов, которые есть
+    public string[] answers;
+
+    // Префабы кнопок с ответами
     public GameObject[] prefabs;
 
-    public bool isMake = false;
+    // Запускать ли скрипт
+    public bool isStart = false;
 
+    // Для работы с массивом answers
     private int answerCount = 0; 
 
     private void Start()
     {
-        if (isMake)
+        if (isStart)
         {
+            // Добавляем UI в окно игры.
             MakeUI(prefabs[0], -1, 145);
             MakeUI(prefabs[1], -1, 27);
             MakeUI(prefabs[2], -268, -56);
@@ -25,43 +34,54 @@ public class AnswerInfo : MonoBehaviour
             MakeUI(prefabs[5], 266, -190);
         }
     }
+
+    // Добавляет UI, принимает Префаб какого-то из элементов UI, его позицию X и Y
     public void MakeUI(GameObject prefab, int posX, int posY)
     {
+        // Добавляем в игру UI
         var p = Instantiate(prefab);
+        // Берем его компонент RectTransform
         var rt = p.GetComponent<RectTransform>();
+        // Задаем родителем askCont
         p.transform.SetParent(transform);
+        // Создаем позицию, в которой должен появиться UI элемент
         p.transform.localPosition = new Vector3(transform.localPosition.x, 0f, transform.localPosition.z);
-        if (posX == -1)
+
+        if (posX == -1) // Если поставлю posX -1 тогда позиция UI по X будет по середине, Y - заданным числом
         {
             rt.anchoredPosition = new Vector2(rt.anchoredPosition.x, posY);
         }
-        else
+        else // Иначе UI появится по строго заданной позиции
         {
             rt.anchoredPosition = new Vector2(posX, posY);
         }
         
+        // Делаем величину по умолчанию
         p.transform.localScale = new Vector3(1f, 1f, 1f);
 
+        // Функция, которая записывает текст в кнопки и в вопрос.
         MakeText(p);
     }
 
     void MakeText(GameObject p)
     {
+        // Находим компонент Text в переданном UI
         var pText = p.GetComponent<Text>();
 
-        if (pText)
+        if (pText)  // Если компонент Text есть, то записываем в него вопрос.
         {
             pText.text = ask;
         }
-        else if(p.GetComponentInChildren<Text>())
+        else if(p.GetComponentInChildren<Text>()) // Если компонент Text лежит в кнопке, то присваеваем ей текст из массива.
         {
             pText = p.GetComponentInChildren<Text>();
             pText.text = answers[answerCount];
+            // Нужен, чтобы каждый раз добавлять следующий текст из массива.
             answerCount++;
         }
         else
         {
-
+            // Ничего не делаем
         }
     }
 }
---
+++ Assets/Scripts/SendScore.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SendScore : MonoBehaviour
+{
+    public void ScoreAssigment(int scoreAnswer)
+    {
+        // Подключаем скрипт, который хранит количество очков.
+        var scoreInfo = GameObject.Find("Canvas").GetComponent<ScoreInfo>();
+        // Добавляем очки за определенный ответ. 1 кнопка - 1 балл, 2 кнопка - 2 балла и т.д.
+        scoreInfo.score += scoreAnswer;
+
+        // Находим askCont и в скрипте берем переменную со следющим контейнером с вопросами и ответами
+        var askCont = transform.parent;
+        var nextAsk = askCont.GetComponent<AnswerInfo>().nextAsk;
+
+        // Если следующий вопрос есть, то включаем видимость следующего контейнера и выключаем текущий.
+        if (nextAsk != null)
+        {
+            nextAsk.SetActive(true);
+            askCont.gameObject.SetActive(false);
+        }
+        else
+        {
+            // Здесь уже конец викторины, поэтому должно открываться окошко с результатами.
+        }
+    }
+}
