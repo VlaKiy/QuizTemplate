5ba7fa9376b5c9b29a6d6e4cf7621a96adf24db9
Revenko Grisha <grisha@revenko.org>
2023-02-14 21:57:00 +0300

    Added AnswerButton

---
+++ Assets/Scripts/AnswerButton.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour
+{
+    [SerializeField] private Text _text;
+    [SerializeField] private Button _button;
+
+    private Answer _answer;
+
+    #region MonoBehaviour
+
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(CheckAnswer);
+    }
+
+    private void OnDisable()
+    {
+        _button.onClick.RemoveAllListeners();
+    }
+
+    #endregion
+
+    public void Init(Answer answer)
+    {
+        _answer = answer;
+        _text.text = _answer.AnswerText;
+    }
+
+    private void CheckAnswer()
+    {
+        if (_answer.IsCorrect)
+        {
+            HandleCorrectAnswer();
+            return;
+        }
+
+        HandleWrongAnswer();
+    }
+
+    private void HandleCorrectAnswer()
+    {
+
+    }
+
+    private void HandleWrongAnswer()
+    {
+
+    }
+}
