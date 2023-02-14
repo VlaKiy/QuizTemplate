24afb254a1e9d5e76f71c4783e6e2b5ef56d1996
Revenko Grisha <grisha@revenko.org>
2023-02-14 21:56:45 +0300

    Added Question

---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+
+public class Question : MonoBehaviour
+{
+    public const int AnswerMaximumAmount = 4;
+
+    [SerializeField] private string _text;
+    [SerializeField] private Answer[] _answers;
+
+    public string Text => _text;
+    public Answer[] Answers => _answers;
+
+    public event Action Answered;
+
+    #region MonoBehaviour
+
+    private void Awake()
+    {
+        if (_answers.Length > AnswerMaximumAmount)
+            throw new Exception("This Amount of answers is not too high!");
+    }
+
+    #endregion
+}
