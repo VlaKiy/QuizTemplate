39b3553617d7f868763a1016fb6c549ea1a0ad21
Revenko Grisha <grisha@revenko.org>
2023-02-16 00:05:56 +0300

    Added PlayerStats

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField] private AnswerChecker _answerChecker;
+
+    private int _score = 0;
+    private int _lifePoints = 3;
+    private int _reward = 1;
+    private int _punishment = 1;
+
+    public event Action OnLost;
+
+    #region MonoBehaviour
+
+    private void OnEnable()
+    {
+        _answerChecker.OnRightAnswered += AddScore;
+        _answerChecker.OnWrongAnswered += TakeLifePoint;
+    }
+
+    #endregion
+
+    private void AddScore() => _score += _reward;
+
+    private void TakeLifePoint()
+    {
+        _lifePoints -= _punishment;
+
+        if (_lifePoints < 0)
+            OnLost?.Invoke();
+    }
+}
