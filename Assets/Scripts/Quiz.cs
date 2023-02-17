a7e1b3f1f519388ecfc31977445f812d22e1c1ac
Revenko Grisha <grisha@revenko.org>
2023-02-17 22:14:56 +0300

    Added QuizView

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityTools;
 
 public class Quiz : MonoBehaviour
 {
-    [SerializeField] private Text _questionText;
     [SerializeField] private PlayerStats _playerStats;
+    [SerializeField] private QuizView _quizView;
     [SerializeField] private Question[] _questionsArray;
-    [SerializeField] private AnswerButton[] _buttons;
  
     private Queue<Question> _questions;
     private Question _currentQuestion;
@@ -54,29 +52,7 @@ public class Quiz : MonoBehaviour
     {
         _currentQuestion = question;
 
-        // Refactor: View class
-        SetupUI();
-    }
-
-    private void SetupUI()
-    {
-        SetupButtons();
-        SetupQuestionText();
-    }
-
-    private void SetupButtons()
-    {
-        var currentAnswers = _currentQuestion.Answers;
-        currentAnswers.TryShuffleArray<Answer>();
-
-        for (var i = 0; i < _buttons.Length; i++)
-            _buttons[i].Init(currentAnswers[i]);
-    }
-
-    private void SetupQuestionText()
-    {
-        var text = _currentQuestion.Text;
-        _questionText.text = text;
+        _quizView.SetupUI(_currentQuestion);
     }
 
     private bool TryGetNextQuestion(out Question question)
