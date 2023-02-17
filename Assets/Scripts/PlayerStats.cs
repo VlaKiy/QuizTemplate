c4c74a5bab8e5563af1217e96d92140be2f81e30
Revenko Grisha <grisha@revenko.org>
2023-02-17 21:26:18 +0300

    Removed life points

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -6,25 +6,20 @@ public class PlayerStats : MonoBehaviour
     [SerializeField] private AnswerChecker _answerChecker;
 
     private int _score = 0;
-    private int _lifePoints = 3;
     private int _reward = 1;
-    private int _punishment = 1;
 
     public event Action OnAnsweredQuestion;
-    public event Action OnLost;
 
     #region MonoBehaviour
 
     private void OnEnable()
     {
         _answerChecker.OnRightAnswered += AddScore;
-        _answerChecker.OnWrongAnswered += TakeLifePoint;
     }
 
     private void OnDisable()
     {
         _answerChecker.OnRightAnswered -= AddScore;
-        _answerChecker.OnWrongAnswered -= TakeLifePoint;
     }
 
     #endregion
@@ -34,15 +29,5 @@ public class PlayerStats : MonoBehaviour
         _score += _reward;
 
         OnAnsweredQuestion?.Invoke();
-    }
-
-    private void TakeLifePoint()
-    {
-        _lifePoints -= _punishment;
-
-        OnAnsweredQuestion?.Invoke();
-
-        if (_lifePoints < 0)
-            OnLost?.Invoke();
     }
 }
