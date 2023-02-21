eab8a541b83e250127f8f1fd2edc86ebcf712a0f
Revenko Grisha <grisha@revenko.org>
2023-02-21 19:21:37 +0300

    Added Restarter

---
+++ Assets/Scripts/Restarter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityTools.Buttons;
+
+public class Restarter : MonoBehaviour
+{
+    [SerializeField] private UIButton _button;
+
+    #region MonoBehaviour
+
+    private void OnEnable()
+    {
+        _button.OnClicked += RestartCurrentScene;
+    }
+
+    private void OnDisable()
+    {
+        _button.OnClicked -= RestartCurrentScene;
+    }
+
+    #endregion
+
+    private void RestartCurrentScene()
+    {
+        var currentScene = SceneManager.GetActiveScene();
+        var currentIndex = currentScene.buildIndex;
+
+        SceneManager.LoadScene(currentIndex);
+    }
+}
